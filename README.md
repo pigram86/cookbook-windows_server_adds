1def60728a6428fa7a3e286ee62dc635cc69d9f8
pigram86 <todd@toddpigram.com>
2014-04-02 07:30:56 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# windows_server_adds cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Todd Pigram (<todd@toddpigram.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'windows_server_adds'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures windows_server_adds'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
